c5c3a9b2d424d1fc9413df0cc8cb84567d97c816
nvaldebenito <nvaldebenito@localhost.localdomain>
2018-12-03 19:34:12 -0300

    Ejercicio 2 100%

--- ejercicio2.rb
+++ ejercicio2.rb
@@ -1,4 +1,17 @@
-productos = {'bebida':850,'chocolate':1200,'galletas':900,'leche':750}
+productos = { 'bebida':850 , 'chocolate':1200 , 'galletas':900 , 'leche':750 }
 productos.each { |producto,valor| puts producto }
 productos.each { |producto,valor| puts valor }
+productos.each { |producto,valor| puts "El #{producto} tiene el siguiente costo #{valor}"}
+
+productos['cereal'] = 2200
+productos.each { |producto,valor| puts "El #{producto} tiene el siguiente costo #{valor}"}
+
+productos[:bebida] = 2000
+productos.each { |producto,valor| puts "El #{producto} tiene el siguiente costo #{valor}"}
+
+
+var_productos = productos.to_a
+print var_productos
+
+productos.delete(:galletas)
 productos.each { |producto,valor| puts "El #{producto} tiene el siguiente costo #{valor}"}
